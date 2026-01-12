2d2aa48f6186f661d92a88fbe39138c3f417d53d
mao0000 <maopuregummy393@gmail.com>
2026-01-13 02:06:00 +0900

    カード管理のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "vehicles#index"
+
+  root "vehicles#index" # ルートパスは車両一覧
+
+  # 車両管理
   resources :vehicles do
+    # 車検管理
     resources :inspections, only: [:create, :destroy]
+  end
+
+  # カード管理
+  resources :cards do
+    member do
+      patch :receive         # 受領ボタン用
+      patch :return_to_stock # 返却ボタン用
+    end
   end
 
   # サイドバーの「車検スケジュール」用
