24546d5a6a4c58fbb9fb4506ab4c8a8bdb174dd8
mao0000 <maopuregummy393@gmail.com>
2026-01-13 02:35:30 +0900

    カード管理コントローラーを作成

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,64 @@
+class CardsController < ApplicationController
+  before_action :set_card, only: [:show, :edit, :update, :destroy, :receive, :return_to_stock]
+
+  def index
+    # 検索やフィルタリングが必要な場合はここで調整
+    @cards = Card.all.includes(:vehicle)
+  end
+
+  def new
+    @card = Card.new
+    # 車両一覧を取得（セレクトボックス用）
+    @vehicles = Vehicle.all
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to cards_path, notice: "カードの発行申請を登録しました。"
+    else
+      @vehicles = Vehicle.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  # 営業所が受領した時の処理
+  def receive
+    if @card.update(status: :active, received_on: Date.today)
+      redirect_to @card, notice: "カードを受領し、利用開始しました。"
+    else
+      redirect_to @card, alert: "受領処理に失敗しました。"
+    end
+  end
+
+  # 本部に返却され在庫に戻る時の処理
+  def return_to_stock
+    if @card.update(status: :stock, vehicle_id: nil, returned_on: Date.today)
+      redirect_to cards_path, notice: "カードが本部へ返却され、在庫（フリー）になりました。"
+    else
+      redirect_to @card, alert: "返却処理に失敗しました。"
+    end
+  end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:vehicle_id, :internal_id, :card_number, :issue_type, :status, :applied_on, :remarks)
+  end
+end
