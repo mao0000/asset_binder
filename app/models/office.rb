a0ceb00c38108a7c4e2185df91567bf0d113c154
mao0000 <maopuregummy393@gmail.com>
2026-01-15 11:57:58 +0900

    営業所リストの変更

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,7 +3,6 @@ class Office < ActiveHash::Base
     { id: 1, name: '東京営業所' },
     { id: 2, name: '千葉営業所' },
     { id: 3, name: '神奈川営業所' },
-    { id: 4, name: '埼玉センター' },
   ]
 
   include ActiveHash::Associations
