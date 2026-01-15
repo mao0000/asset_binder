1b5d23d2a00466dd1b87344fd7265deda948f1c4
mao0000 <maopuregummy393@gmail.com>
2026-01-15 23:57:56 +0900

    給油管理　index・importアクション

--- app/controllers/fuels_controller.rb
+++ app/controllers/fuels_controller.rb
@@ -1,2 +1,12 @@
 class FuelsController < ApplicationController
+  # 給油データ一覧
+  def index
+    @fuels = Fuel.all.order(filled_at: :desc)
+  end
+
+  # 給油データのインポート
+  def import
+    Fuel.import(params[:file])
+    redirect_to fuels_path, notice: "給油データをインポートしました。"
+  end
 end
