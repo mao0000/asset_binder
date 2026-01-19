ca64e18bfa3c5bdcf00cd2349f9088fdee727104
mao0000 <maopuregummy393@gmail.com>
2026-01-19 11:36:20 +0900

    Basic認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '1111'
+    end
+  end
+
   protected
   
   def after_sign_in_path_for(resource)
